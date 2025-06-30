83bf137a3ff577898a05b59073f6629512e2265e
archways404 <archways@gmx.us>
2025-06-30 08:53:11 +0200

    init

--- components/printers.ps1
+++ components/printers.ps1
@@ -1,27 +1,35 @@
 function Add-Printers {
-    $printerList = @{
-        "1" = "\\SEMA3-util-p01\SEMA3-FollowMe"
-        "2" = "\\SEMA3-util-p01\SEMA3-Plot-PD"
-        "3" = "\\SEMA3-util-p01\DKBAL-Print Room"
-        "4" = "\\SEMA3-util-p01\NOOS2-NH8"
+    $printerPrefix = "\\SEMA3-util-p01\"
+
+    $printerNames = @{
+        "1" = "SEMA3-FollowMe"
+        "2" = "SEMA3-Plot-PD"
+        "3" = "DKBAL-Print Room"
+        "4" = "NOOS2-NH8"
         "5" = "Custom"
     }
 
     Log -Level HEADER -Message "Available Printers"
-    foreach ($key in $printerList.Keys) {
-        Write-Host "$key. $($printerList[$key])" -ForegroundColor White
+    foreach ($key in ($printerNames.Keys | Sort-Object {[int]$_})) {
+        $label = $printerNames[$key]
+        if ($label -ne "Custom") {
+            Write-Host "$key. $label" -ForegroundColor White
+        } else {
+            Write-Host "$key. Custom (manual input)" -ForegroundColor White
+        }
     }
 
     $choices = Read-Host "`nEnter the number(s) of printer(s) to add (comma-separated, e.g. 1,3,5)"
     $selected = $choices -split ',' | ForEach-Object { $_.Trim() }
 
     foreach ($option in $selected) {
-        if ($printerList.ContainsKey($option)) {
-            if ($printerList[$option] -eq "Custom") {
+        if ($printerNames.ContainsKey($option)) {
+            if ($printerNames[$option] -eq "Custom") {
                 $customPrinter = Read-Host "Enter full UNC path to printer (e.g. \\server\printer)"
                 Try-AddPrinter $customPrinter
             } else {
-                Try-AddPrinter $printerList[$option]
+                $fullShare = "$printerPrefix$($printerNames[$option])"
+                Try-AddPrinter $fullShare
             }
         } else {
             Log -Level WARN -Message "Invalid selection: $option"
